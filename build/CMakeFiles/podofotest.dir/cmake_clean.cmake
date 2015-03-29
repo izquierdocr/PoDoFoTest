cc8f5a6a3c49361fc31cb745e9d5f2b2b1d7b5de
Ramón Izquierdo Córdova <izquierdocr@hotmail.com>
2015-03-29 01:28:47 -0600

    Initial commit of the project

---
+++ build/CMakeFiles/podofotest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/podofotest.dir/main.cpp.o"
+  "podofotest.pdb"
+  "podofotest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/podofotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
